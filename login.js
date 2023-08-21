a8f5b89572dd0780b61f8ac54a56beb7ee768a3e
Rafaela Vitória Port <rafaelavitoriaport1590@gmail.com>
2023-08-21 18:02:44 -0300

    commit um

---
+++ login.js
@@ -0,0 +1,11 @@
+let apelido;
+
+function troca(){
+    window.location.href = "file:///C:/Users/rafae/OneDrive/%C3%81rea%20de%20Trabalho/design/chat/telas/indexabas.html"
+}
+
+function puxarApelido(apelido){
+    document.getElementById("apelido").value = apelido;
+}
+
+
